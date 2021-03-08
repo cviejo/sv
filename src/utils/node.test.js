00bc74bebd0e131fd5443e24b07f6d5ab8cb4c0a
cviejo <carlos.viejo.muros@gmail.com>
2021-03-08 23:49:45 +0100

    removed unused utils

--- src/utils/node.test.js
+++
@@ -1,8 +0,0 @@
-import { ioCount } from './node.js';
-
-test('ioCount', () => {
-	expect(ioCount(1)).toEqual(1);
-	expect(ioCount(2)).toEqual(2);
-	expect(ioCount(3)).toEqual(1);
-	expect(ioCount(4)).toEqual(2);
-});
