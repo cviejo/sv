4167cd8c177328338385095793a5b3db0156fd9d
cviejo <carlos.viejo.muros@gmail.com>
2021-03-08 17:36:00 +0100

    added append

--- src/utils/string.js
+++ src/utils/string.js
@@ -2,9 +2,11 @@ import { replace, __, curry } from 'ramda';
 
 const remove = replace(__, '');
 
+const append = curry((a, b) => `${b}${a}`);
+
 const log = curry((label, x) => {
 	console.log(label, x);
 	return x;
 });
 
-export { remove, log };
+export { remove, log, append };
