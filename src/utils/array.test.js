fa490c13062144968e529256e044d1adaf733c8c
cviejo <carlos.viejo.muros@gmail.com>
2021-03-13 15:57:30 +0100

    removed array utils

--- src/utils/array.test.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-import { findEq } from './array';
-
-test('findEq', () => {
-	const ok = { a: 1, b: 2, ok: 1 };
-
-	expect(findEq({ a: 1, b: 2 }, [{ a: 1 }, { b: 1 }, ok])).toEqual(ok);
-});
