357dcade5d6f0db76e42ef1485d3c31a20d55369
cviejo <carlos.viejo.muros@gmail.com>
2021-03-15 09:35:16 +0100

    formatting

--- src/stores/focus.js
+++ src/stores/focus.js
@@ -1,17 +1,17 @@
 import { curry, pipe, __, prop, tap, nth, indexOf } from 'ramda';
 import { writable, get } from 'svelte/store';
 import { safe } from '../utils/object.js';
-import { findByProp } from '../utils/array.js';
+import { findByProp } from '../utils/list.js';
 
 const store = writable();
 
 const targets = [];
 
-const focusNode = tap((x) => x.node.focus());
+const focusNode = tap(x => x.node.focus());
 
 const findById = findByProp('id', __, targets);
 
-const nextIndex = (x) => (x + 1) % targets.length;
+const nextIndex = x => (x + 1) % targets.length;
 
 const getFocus = () => get(store);
 
@@ -21,8 +21,8 @@ const next = pipe(getFocus, indexOf(__, targets), nextIndex, nth(__, targets), p
 
 const register = curry((node, id) => {
 	['addEventListener', 'on']
-		.filter((x) => node[x])
-		.forEach((x) => node[x]('mousedown', () => set(id)));
+		.filter(x => node[x])
+		.forEach(x => node[x]('mousedown', () => set(id)));
 
 	targets.push({ node, id });
 });
