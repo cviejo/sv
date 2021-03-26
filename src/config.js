0c98386ca6b5f16fcf286be569c60af7dbb930e0
cviejo <carlos.viejo.muros@gmail.com>
2021-03-26 16:29:39 +0100

    fixed combinator reference

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 const step = 25;
 const region = step * 10;
-const sizes = {
-	step,
-	region,
-	grid: region * 10,
-};
+const grid = region * 10;
+
+const sizes = { step, region, grid };
 
 export { sizes };
--- src/utils/object.js
+++ src/utils/object.js
@@ -1,10 +1,19 @@
 import { unless, isNil, useWith, split, pick, propOr, curry, path, pipe, last } from 'ramda';
-import { map, valuesIn, toPairsIn, is, bind } from 'ramda';
-import { I } from './combinators.js';
+import { map, valuesIn, mergeRight, toPairsIn, is, bind } from 'ramda';
+import { S, I } from './combinators.js';
+
+const bindAll = obj =>
+	toPairsIn(obj)
+		.filter(pipe(last, is(Function)))
+		.reduce((acc, [key, value]) => ({ ...acc, [key]: bind(value, obj) }), {});
 
 const dotPath = useWith(path, [split('.'), I]);
 
-const safe = unless(isNil);
+const mergeResult = S(mergeRight);
+
+const pickRename = curry((keys, obj) => pick(valuesIn(keys), renameKeys(keys, obj)));
+
+const pickWith = curry((props, fn, x) => pipe(pick(props), map(fn))(x));
 
 const propOrKey = curry((key, x) => propOr(key, key, x));
 
@@ -12,13 +21,6 @@ const renameKeys = curry((keys, obj) =>
 	toPairsIn(obj).reduce((acc, [key, value]) => ({ ...acc, [propOrKey(key, keys)]: value }), {})
 );
 
-const pickRename = curry((keys, obj) => pick(valuesIn(keys), renameKeys(keys, obj)));
+const safe = unless(isNil);
 
-const pickWith = curry((props, fn, x) => pipe(pick(props), map(fn))(x));
-
-const bindAll = obj =>
-	toPairsIn(obj)
-		.filter(pipe(last, is(Function)))
-		.reduce((acc, [key, value]) => ({ ...acc, [key]: bind(value, obj) }), {});
-
-export { dotPath as path, toPairsIn, safe, bindAll, renameKeys, pickRename, pickWith };
+export { dotPath as path, toPairsIn, safe, bindAll, renameKeys, pickRename, mergeResult, pickWith };
