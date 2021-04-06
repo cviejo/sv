dde8576909b24c981bb5d674fe5c7d265641f24a
cviejo <carlos.viejo.muros@gmail.com>
2021-04-07 00:16:33 +0200

    hints util

--- src/utils/string.js
+++ src/utils/string.js
@@ -9,4 +9,18 @@ const log = curry((label, x) => {
 	return x;
 });
 
-export { remove, log, append };
+// code taken from: https://github.com/philc/vimium
+const hints = count => {
+	const hintChars = 'sadfjklewcmpgh';
+	const xs = [''];
+	let offset = 0;
+	while (xs.length - offset < count || xs.length === 1) {
+		const hint = xs[offset++];
+		for (let ch of hintChars) {
+			xs.push(ch + hint);
+		}
+	}
+	return xs.slice(offset, offset + count);
+};
+
+export { remove, log, append, hints };
