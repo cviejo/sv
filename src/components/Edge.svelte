69888afdd90d66bfb574e12138fcbccd3218f137
cviejo <carlos.viejo.muros@gmail.com>
2021-04-20 19:24:50 +0200

    refactored Edge

--- src/components/Edge.svelte
+++ src/components/Edge.svelte
@@ -1,25 +1,29 @@
 <script>
 	import { onMount, tick } from 'svelte';
+	import { writable } from 'svelte/store';
 	import { nodes } from '../stores.js';
 	import { center } from '../utils/dom';
 
 	export let edge;
 
-	let path = '';
-
-	const update = (a, b) => {
-		const mod = Math.abs(b.y - a.y) / 2.2;
-		path = `M ${a.x},${a.y} C ${a.x},${a.y + mod} ${b.x},${b.y - mod} ${b.x},${b.y}`;
-	};
-
 	const from = nodes.byId(edge.from);
+
 	const to = nodes.byId(edge.to);
 
-	// node as store to react to position changes
+	const path = writable('');
+
+	const pathMod = (a, b) => Math.abs(b.y - a.y) / 2.2;
+
+	const data = (a, b, mod) =>
+		`M ${a.x},${a.y} C ${a.x},${a.y + mod} ${b.x},${b.y - mod} ${b.x},${b.y}`;
+
+	const update = (a, b) => path.set(data(a, b, pathMod(a, b)));
+
 	$: outlet = $from.outlets[edge.outlet];
+
 	$: inlet = $to.inlets[edge.inlet];
 
-	// tick to finish both nodes move
+	// tick to finish both node movements
 	$: tick().then(() => update(center(outlet.elem), center(inlet.elem)));
 
 	onMount(() => {
@@ -37,6 +41,6 @@
 	}
 </style>
 
-{#if path}
-	<path d={path} />
+{#if $path}
+	<path d={$path} />
 {/if}
